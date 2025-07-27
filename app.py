541a28bb63c705fb97f15d2169fd53abc491ef5b
arunkamaraj2345 <arunkamaraj2345@gmail.com>
2025-07-27 16:41:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'YFinance API is Running!'
+
+@app.route('/get_stock_data', methods=['GET'])
+def get_stock_data():
+    symbol = request.args.get('symbol')
+    if not symbol:
+        return jsonify({"error": "No symbol provided."})
+
+    try:
+        ticker = yf.Ticker(symbol)
+        df = ticker.history(period="20d").reset_index()
+
+        if df.empty:
+            return jsonify({"error": f"No data found for {symbol}."})
+
+        result = [["Date", "Close"]]
+        for index, row in df.iterrows():
+            result.append([row['Date'].strftime('%Y-%m-%d'), row['Close']])
+
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
